fd23caa8babc7468b0f83ddae966d035400e84f2
poovarasansivan <poovarasansivan3@gmail.com>
2025-04-01 10:29:32 +0530

    factorial program with documentation

---
+++ task-1/Program.cs
@@ -0,0 +1,29 @@
+﻿
+namespace task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string? input = Console.ReadLine();
+            if (!int.TryParse(input, out int n))
+            {
+                Console.WriteLine("Error! Invalid input.");
+                return;
+            }
+            int fact = 1;
+
+            if (n < 0)
+            {
+                Console.WriteLine("Error! Given number is negative.");
+                return;
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine(fact);
+        }
+    }
+}
